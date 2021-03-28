b56a332a25c9bf18ece01f1bbcb9a342d1ebd46e
chaule01 <chaule01@gmail.com>
2021-03-28 10:11:44 +0700

    Clean code

--- seleniumfw/src/main/java/com/kr/app/testcase/LoginRun.java
+++ seleniumfw/src/main/java/com/kr/app/testcase/LoginRun.java
@@ -9,43 +9,43 @@ import supports.CommonFunctions;
 
 public class LoginRun {
 
-//	@Test
-//	public void testLoginSucess() throws InterruptedException {
-//		CommonFunctions.setBrowser("chrome");
-//		Action page = new Action(CommonFunctions.getDriver());
-//		page.openApplication();
-//		page.testLoginSuccess();
-//		Thread.sleep(1000);
-//		CommonFunctions.closeBrowser();
-//
-//	}
-//
-//	@Test
-//	public void testFilterSchoolbyName() throws InterruptedException {
-//		CommonFunctions.setBrowser("chrome");
-//		Action page = new Action(CommonFunctions.getDriver());
-//		page.openApplication();
-//		page.testLoginSuccess();
-//		Thread.sleep(1000);
-//		page.FilterSchoolbyName();
-//		Thread.sleep(1000);
-//		CommonFunctions.waitForElementPresence("xpath", "//mat-cell[contains(.,'TEST SCHOOL 8')]");
-//		CommonFunctions.closeBrowser();
-//	}
-//	
-//	@Test
-//	public void testFilterSchoolbyCode() throws InterruptedException {
-//		CommonFunctions.setBrowser("chrome");
-//		Action page = new Action(CommonFunctions.getDriver());
-//		page.openApplication();
-//		page.testLoginSuccess();
-//		Thread.sleep(1000);
-//		page.FilterSchoolbyCode();
-//		Thread.sleep(1000);
-//		CommonFunctions.waitForElementPresence("xpath", "//mat-cell[contains(.,'200')]");
-//		CommonFunctions.closeBrowser();
-//	}
-//	
+	@Test
+	public void testLoginSucess() throws InterruptedException {
+		CommonFunctions.setBrowser("chrome");
+		Action page = new Action(CommonFunctions.getDriver());
+		page.openApplication();
+		page.testLoginSuccess();
+		Thread.sleep(1000);
+		CommonFunctions.closeBrowser();
+
+	}
+
+	@Test
+	public void testFilterSchoolbyName() throws InterruptedException {
+		CommonFunctions.setBrowser("chrome");
+		Action page = new Action(CommonFunctions.getDriver());
+		page.openApplication();
+		page.testLoginSuccess();
+		Thread.sleep(1000);
+		page.FilterSchoolbyName();
+		Thread.sleep(1000);
+		CommonFunctions.waitForElementPresence("xpath", "//mat-cell[contains(.,'TEST SCHOOL 8')]");
+		CommonFunctions.closeBrowser();
+	}
+	
+	@Test
+	public void testFilterSchoolbyCode() throws InterruptedException {
+		CommonFunctions.setBrowser("chrome");
+		Action page = new Action(CommonFunctions.getDriver());
+		page.openApplication();
+		page.testLoginSuccess();
+		Thread.sleep(1000);
+		page.FilterSchoolbyCode();
+		Thread.sleep(1000);
+		CommonFunctions.waitForElementPresence("xpath", "//mat-cell[contains(.,'200')]");
+		CommonFunctions.closeBrowser();
+	}
+	
 	@Test
 	public void testAddStudent() throws InterruptedException {
 		CommonFunctions.setBrowser("chrome");
